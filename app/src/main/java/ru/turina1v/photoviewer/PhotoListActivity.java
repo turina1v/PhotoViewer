1ffb8b1de363be380d1755066f31b6ea548948de
Valentina Tiurina <turina.valentina@gmail.com>
2020-05-13 19:44:50 +0300

    Initial commit

---
+++ app/src/main/java/ru/turina1v/photoviewer/PhotoListActivity.java
@@ -0,0 +1,14 @@
+package ru.turina1v.photoviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_list);
+    }
+}
