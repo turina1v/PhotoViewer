99475741a8d38f0d75fc304f49fc1934d72dd68d
Valentina Tiurina <turina.valentina@gmail.com>
2020-06-08 13:52:13 +0300

    Удалены лишние классы, связанные с активити для обрезки фото

--- app/src/main/java/ru/turina1v/photoviewer/presenter/CropImagePresenter.java
+++
@@ -1,9 +0,0 @@
-package ru.turina1v.photoviewer.presenter;
-
-import moxy.InjectViewState;
-import moxy.MvpPresenter;
-import ru.turina1v.photoviewer.view.cropimage.CropImageView;
-
-@InjectViewState
-public class CropImagePresenter extends MvpPresenter<CropImageView> {
-}
--- app/src/main/java/ru/turina1v/photoviewer/view/cropimage/CropImageView.java
+++
@@ -1,6 +0,0 @@
-package ru.turina1v.photoviewer.view.cropimage;
-
-import moxy.MvpView;
-
-public interface CropImageView extends MvpView {
-}
